cd64b463a7e78b06986514939392052ebfddcbae
Katlin <Katlin>
2019-03-29 09:57:07 -0700

    add comments to create basic structure of quiz

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function findAverage(num1, num2, num3, num4) {
+  var average = (num1 + num2 + num3 + num4) / 4;
+  return Math.round(average);
+}
+
+$(function() {
+  //if user selects front end, show CSS/Design Track
+  //else show the next questions
+
+  //submit
+    //get average of answers and show coresponding track
+
+
+});
